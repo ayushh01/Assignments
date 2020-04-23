4d036ca32fbb50dd3aad1b9297346bad3748b1a2
Ayush Srivastava <srivastavaayush.alld@gmail.com>
2020-04-23 22:39:26 +0530

    added express

--- exp.js
+++ exp.js
@@ -1,13 +1,16 @@
 const express  = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const app = express();
 
 const hostname ='localhost';
 const port = 3000;
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
 app.use((req,res,next)=>{
-    console.log(req.headers);
     res.statusCode=200;
     res.setHeader('Content-type','text/html');
     res.end('<html><body><h1>This is express server</h1></body></html>');
