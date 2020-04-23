d5fc99aad8c2b97e25119e7ae63e61988f1d78f2
Ayush Srivastava <srivastavaayush.alld@gmail.com>
2020-04-23 19:51:59 +0530

    updated server

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res)=>{
-    console.log(req.headers);
+    console.log("Requested for " + req.url + " method is " + req.method)
 
-    res.statusCode = 200;
-    res.setHeader('Content-type' , 'text/html');
-    res.end('<html><body><h1>Hello World</h1></body></html>');
+    if(req.method == 'GET')
+    {
+        var fileUrl;
+        if(req.url == '/')
+        {
+            fileUrl = '/index.html';
+        }
+        else
+        {
+            fileUrl = req.url;
+        }
+
+        var filePath = path.resolve('./public'+fileUrl);
+        var fileExt = path.extname(filePath);
+        if(fileExt == '.html')
+        {
+            fs.exists(filePath , (exists)=>{
+                if(!exists)
+                {
+                    res.statusCode = 404;
+                    res.setHeader('Content-type','text/html');
+                    res.end('<html><body><h1>Error 404: '+ req.url + ' file not found</h1></body></html>');
+                }
+                else
+                {
+                    res.statusCode = 200;
+                    res.setHeader('Content-type','text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                }
+            })
+
+        }
+        else
+        {
+            res.statusCode = 404;
+            res.setHeader('Content-type','text/html');
+            res.end('<html><body><h1>Error 404: '+ req.url + ' file not html file</h1></body></html>');
+        }
+
+
+    }
+    else
+    {
+        res.statusCode = 404;
+        res.setHeader('Content-type','text/html');
+        res.end('<html><body><h1>Error 404: '+ req.method + ' Method is not GET</h1></body></html>');
+    }
 })
 
 server.listen(port ,hostname ,()=>{
-    console.log(`Server is running at http://${hostname}:${port}`);
+    console.log(`Server is running at http://${hostname}:${port}`)
 });
