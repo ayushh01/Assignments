8def3ceb0fe16155c010e87ab8aaa97f6268730b
Ayush Srivastava <srivastavaayush.alld@gmail.com>
2020-05-04 12:59:30 +0530

    added express-session

---
+++ Assignment-3/models/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    admin:{
+        type:Boolean,
+        default:false
+    }
+});
+
+var User = mongoose.model('User' , UserSchema);
+
+module.exports = User;
